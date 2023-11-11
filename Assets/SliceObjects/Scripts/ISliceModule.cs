9052769b0e741cfe24448bec6736f0bd856e83de
InKres <Maxshalol1@mail.ru>
2023-11-12 01:27:42 +0500

    Added interface ISliceModule and module Slice

---
+++ Assets/SliceObjects/Scripts/ISliceModule.cs
@@ -0,0 +1,8 @@
+﻿public interface ISliceModule
+{
+    /// <summary>
+    /// Change the state of the object section
+    /// </summary>
+    /// <param name="State"></param>
+    void SliceObject(bool State);
+}
---
+++ Assets/SliceObjects/Scripts/Slice.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Slice : MonoBehaviour, ISliceModule
+{
+    [Header("Components")]
+    public GameObject fullObject;
+    
+    [Space]
+    public GameObject halfObject;
+
+    public void SliceObject(bool State)
+    {
+        if(fullObject == null)
+        {
+            Debug.LogError("FullObject is NULL!");
+            return;
+        }
+
+        if(halfObject == null)
+        {
+            Debug.LogError("HalfObject is NULL!");
+            return;
+        }
+
+        fullObject.SetActive(!State);
+        halfObject.SetActive(State);
+    }
+}
